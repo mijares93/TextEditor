82fd9b75d54dadb0d67ca347e3700002b69cfb9d
mijares93 <mijares9329@gmail.com>
2019-07-26 19:51:35 -0500

    Code refactoring

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -13,6 +13,19 @@ import javax.swing.JTextField;
 public class MainWindow {
 
 	private JFrame frame;
+	private JTextPane textPane;
+	private JMenuBar menuBar;
+	private JMenuItem mntmNewMenuItem;
+	private JMenuItem mntmNewMenuItem_1;
+	private JMenuItem mntmNewMenuItem_2;
+	private JMenuItem mntmNewMenuItem_3;
+	private JMenuItem mntmNewMenuItem_4;
+	private JMenuItem mntmNewMenuItem_5;
+	
+	
+	
+	
+	
 
 	/**
 	 * Launch the application.
@@ -46,38 +59,38 @@ public class MainWindow {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JTextPane textPane = new JTextPane();
+		textPane = new JTextPane();
 		textPane.setBounds(0, 0, 450, 256);
 		frame.getContentPane().add(textPane);
 		
-		JMenuBar menuBar = new JMenuBar();
+		menuBar = new JMenuBar();
 		frame.setJMenuBar(menuBar);
 		
 		JMenu mnNewMenu = new JMenu("File");
 		menuBar.add(mnNewMenu);
 		
-		JMenuItem mntmNewMenuItem = new JMenuItem("Open File");
+		mntmNewMenuItem = new JMenuItem("Open File");
 		mnNewMenu.add(mntmNewMenuItem);
 		
-		JMenuItem mntmNewMenuItem_2 = new JMenuItem("Save File");
+		mntmNewMenuItem_2 = new JMenuItem("Save File");
 		mnNewMenu.add(mntmNewMenuItem_2);
 		
-		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Close File");
+		mntmNewMenuItem_1 = new JMenuItem("Close File");
 		mnNewMenu.add(mntmNewMenuItem_1);
 		
 		JMenu mnNewMenu_2 = new JMenu("Edit");
 		menuBar.add(mnNewMenu_2);
 		
-		JMenuItem mntmNewMenuItem_4 = new JMenuItem("Undo - Ctrl-Z");
+		mntmNewMenuItem_4 = new JMenuItem("Undo - Ctrl-Z");
 		mnNewMenu_2.add(mntmNewMenuItem_4);
 		
-		JMenuItem mntmNewMenuItem_5 = new JMenuItem("Redo - Ctrl-Y");
+		mntmNewMenuItem_5 = new JMenuItem("Redo - Ctrl-Y");
 		mnNewMenu_2.add(mntmNewMenuItem_5);
 		
 		JMenu mnNewMenu_1 = new JMenu("About");
 		menuBar.add(mnNewMenu_1);
 		
-		JMenuItem mntmNewMenuItem_3 = new JMenuItem("About...");
+		mntmNewMenuItem_3 = new JMenuItem("About...");
 		mnNewMenu_1.add(mntmNewMenuItem_3);
 	}
 }
